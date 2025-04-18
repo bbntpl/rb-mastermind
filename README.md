23e7f154a4dc7bd75f7fc28ac2ed2115bf9e63eb
bbntpl <beaver@bbntpl>
2025-04-18 03:50:13 -0400

    docs: add program desc

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # rb-mastermind
+The following configurations: 
+- Players may configure between 6-8 colors to choose from. 
+- A player may choose to play against a computer.
+- A player may choose the available number of holes to insert the colored objects between 4-5
+
+Unchangeable rules:
+- Only 10 retries for each codebreaker
+- There is only 4 available feedback holes given to the codebreaker.
+
+For feedback holes:
+White represents a correctly guessed color, but not in the correct hole.
+Pink represents a correctly guessed color slotted on the correct hole.
+
+Note: Feedback holes are not associated with any holes in which a colored object is slotted. They merely represent a seat for available feedback given to the user, which means that they are not ordered based on the order of the inserted colored objects. 
---
+++ main.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Pseudocode for Mastermind program
+#
+# 1. Configure the game settings such as # of colors, # of holes, and modes (PvP, PvCOM, or COMvCOM)
+# 2. The player(s) must choose between their roles of being the codemaker or the codebreaker
+# 3. The codebreaker must guess pattern of colored objects in a row given by the codemaker.
+# 4. The codemaker must check the patterns of colored objects inserted in a row.
+# 4. Answer yourself this question: Has the game ended?
+#     a. If so, go to Step 7
+#     b. Otherwise, continue
+# 5. Answer yourself this question: Did some conditions changed?
+#     a. If so, show feedback objects based on the # of conditions (either white or pink; pink because I'm color blind)
+#     b. Otherwise, don't do anything
+# 6. Go back to Step 3
+# 7. Declare the winner (save the score),
+#     a. If 0 retries left and the codes aren't matched, codemaker wins!
+#     b. If codes matched, codebreaker wins!
+# 8. Ask the user to play again
+#     a. Yes? Go to Step 2
+#     b. Yes, but change the settings. Go to Step 1
+#     c. No, end the program, yay!
